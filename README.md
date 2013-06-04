50ad53b4be21785d19d8b25aeb28baad4e3523ce
Joshua Buysse <buysse@umn.edu>
2013-06-03 22:49:06 -0500

    Initial packages

---
+++ README.md
@@ -0,0 +1,13 @@
+# fvwm2 cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Joshua Buysse (<buysse@gmail.com>)
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: fvwm2
+# Recipe:: default
+#
+# Copyright (C) 2013 Joshua Buysse
+# Licensed under Apache v2.0 License
+#
+
+package "fvwm2"
+package "fvwm-icons"
+package "stalonetray"
